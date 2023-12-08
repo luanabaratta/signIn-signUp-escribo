401fff445c0fcc13fe0081b394072c7c2e3e4996
Luana B. dos Santos <luanabaratta@Luanas-MacBook-Air.local>
2023-12-08 00:04:50 -0300

    files added to gitignore

--- backend/server.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const port = 5000;
-
-const app = express();
-
-app.get('/', (req, res) => res.send('Server is ready'));
-
-app.listen(port, () => console.log(`Server started on port ${port}`));
