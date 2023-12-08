3caebc33a495379f9ffa0157aff61707585e8285
Luana B. dos Santos <luanabaratta@Luanas-MacBook-Air.local>
2023-12-08 18:05:31 -0300

    connect to user routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,44 @@
 import asyncHandler from 'express-async-handler';
 
-
-
-/***** POST /api/usuarios/auth *****/
+// @desc Auth user/set token
+// route POST /api/usuarios/auth
+// @access Public
 const authUser = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'Autenticação de Usuário' });
 });
 
-export { authUser };
+// @desc Register a new user
+// route POST /api/usuarios
+// @access Public
+const registerUser = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: 'Registrar Usuário' });
+});
+
+// @desc Logout user
+// route POST /api/usuarios/logout
+// @access Public
+const logoutUser = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: 'Logout de Usuário' });
+});
+
+// @desc Get user profile
+// route GET /api/usuarios/perfil
+// @access Private
+const getUserProfile = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: 'Perfil do Usuário' });
+});
+
+// @desc Update user profile
+// route PUT /api/usuarios/perfil
+// @access Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: 'Atualizar o perfil' });
+});
+
+export {
+    authUser,
+    registerUser,
+    logoutUser,
+    getUserProfile,
+    updateUserProfile
+};
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,17 @@
 import express from 'express';
-import dotenv from 'dotenv'; dotenv.config();
-import userRoutes from "./routes/userRoutes.js";
-
+import dotenv from 'dotenv';
+dotenv.config();
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 const port = process.env.PORT || 5000;
+import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
 
 app.use('/api/usuarios', userRoutes);
 
 app.get('/', (req, res) => res.send('Servidor rodando'));
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
