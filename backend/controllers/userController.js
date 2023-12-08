6fcfb25fad59868010d4610472a91ddfd55338be
Luana B. dos Santos <luanabaratta@Luanas-MacBook-Air.local>
2023-12-08 01:17:57 -0300

    auth msg

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,5 @@
 const authUser = async (req, res) => {
-    res.status(200).json({ message: 'Autenticar Usuário' });
+    res.status(200).json({ message: 'Autenticação de Usuário' });
 };
 
 export { authUser };
