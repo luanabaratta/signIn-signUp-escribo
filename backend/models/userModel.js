c1bcf5234c5bd1af34d312c2971e5f45ee5326b7
Luana B. dos Santos <luanabaratta@Luanas-MacBook-Air.local>
2023-12-10 01:01:57 -0300

    User schema has been created

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,3 +1,36 @@
 import mongoose from "mongoose";
 
-const userSchema =
+const phoneSchema = mongoose.Schema({
+    number: {
+        type: String,
+        required: true
+    },
+    ddd: {
+        type: String,
+        required: true
+    }
+});
+
+const userSchema = mongoose.Schema({
+   name: {
+       type: String,
+       required: true
+   },
+   email: {
+       type: String,
+       required: true,
+       unique: true
+   },
+   password: {
+       type: String,
+       required: true
+   },
+   phones: [phoneSchema]
+}, {
+    timestamps: true
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
+
