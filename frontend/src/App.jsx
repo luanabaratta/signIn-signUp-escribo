01d0a1479b836a50e36e3ed69fec65621f3c5d4b
Luana B. dos Santos <luanabaratta@Luanas-MacBook-Air.local>
2023-12-11 12:34:12 -0300

    react-router and outlet added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
+import { Outlet } from 'react-router-dom';
+import { Container } from "react-bootstrap";
 import Header from "./components/Header.jsx";
-import HomeScreen from "./screens/HomeScreen.jsx";
 
 const App = () => {
     return (
         <>
             <Header />
-            <HomeScreen />
+            <Container className='my-2'>
+                <Outlet />
+            </Container>
         </>
     );
 };
