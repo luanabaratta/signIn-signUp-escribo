d981130845a521cc78efde9b2d276f5225a0fcd7
Luana B. dos Santos <luanabaratta@Luanas-MacBook-Air.local>
2023-12-11 18:49:53 -0300

    fixing vercel backend

--- backend/server.js
+++ backend/server.js
@@ -21,10 +21,10 @@ app.use('/api/usuarios', userRoutes);
 
 if (process.env.NODE_ENV === 'production') {
     const __dirname = path.resolve();
-    app.use(express.static(path.join(__dirname, '/frontend/dist')));
+    app.use(express.static(path.join(__dirname, '/frontend/')));
 
     app.get('*', (req, res) =>
-        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+        res.sendFile(path.resolve(__dirname, 'frontend'))
     );
 } else {
     app.get('/', (req, res) => {
