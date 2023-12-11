1e17ab533ce32576a148ecf3eb65a6a1a793ee21
Luana B. dos Santos <luanabaratta@Luanas-MacBook-Air.local>
2023-12-11 01:30:01 -0300

    logout impl. destroying the cookie

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -52,7 +52,12 @@ const registerUser = asyncHandler(async (req, res) => {
 
 // api/usuarios/sair @access Public
 const logoutUser = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Logout de Usuário' });
+    res.cookie('jwt', '', {
+       httpOnly: true,
+       expires: new Date(0)
+    });
+
+    res.status(200).json({ message: 'Usuário deslogado' });
 });
 
 // api/usuarios/perfil @access Private
