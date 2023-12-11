c5828db956080dce0527efbce87f84bcf737f3d4
Luana B. dos Santos <luanabaratta@Luanas-MacBook-Air.local>
2023-12-11 03:08:26 -0300

    frontend started

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header.jsx";
+import HomeScreen from "./screens/HomeScreen.jsx";
+
+const App = () => {
+    return (
+        <>
+            <Header />
+            <HomeScreen />
+        </>
+    );
+};
+
+export default App;
