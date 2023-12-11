97dea755f87065d4d940ab0192bccae74bfb8ad7
Luana B. dos Santos <luanabaratta@Luanas-MacBook-Air.local>
2023-12-11 18:39:23 -0300

    adding production config to the server

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,19 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use('/api/usuarios', userRoutes);
-app.get('/', (req, res) => res.send('Servidor rodando'));
+
+if (process.env.NODE_ENV === 'production') {
+    const __dirname = path.resolve();
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+    );
+} else {
+    app.get('/', (req, res) => {
+        res.send('API rodando....');
+    });
+}
 
 app.use(notFound);
 app.use(errorHandler);
